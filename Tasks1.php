b2fe4a1b5f9b165993505ce6e1ac8b50bc067fab
MalangaVishwajith <Malangavishwajiththilakarathna@gmail.com>
2023-09-28 19:37:39 +0530

    Create Tasks1.php

---
+++ Tasks1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Program Output</title>
+</head>
+<body>
+    <div>
+        <h1>Task number one: Write a PHP program to loop from 7 to 21 and print numbers.</h1>
+    </div>
+
+    <div>
+        <h2>Output:</h2>
+        <p>
+<?php
+for ($i = 7; $i <= 21; $i++) {
+    echo $i . " ";
+}
+?>
+        </p>
+    </div>
+</body>
+</html>
