2d6e7fd03d22d170e8ad759bf4bba3d6f78656c2
MalangaVishwajith <Malangavishwajiththilakarathna@gmail.com>
2023-10-01 17:52:29 +0530

    task3

---
+++ Task3.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Day of the Week</title>
+</head>
+<body>
+    <h1>Today is <?php
+        
+        $dayOfWeek = date("l");
+        
+        
+        echo $dayOfWeek;
+    ?></h1>
+</body>
+</html>
